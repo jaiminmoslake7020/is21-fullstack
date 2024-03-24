18a77195d16fcabf406bdd990537eb82b9069d05
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-03-24 15:08:07 -0700

    adding dummy data for frontend

--- packages/frontend/src/data/colour-list.tsx
+++ packages/frontend/src/data/colour-list.tsx
@@ -1,1 +1,1 @@
-export const colourList = ['blue', 'grey', 'black', 'white', 'purple']
+export const colourList = ['blue', 'gray', 'black', 'white', 'purple']
--- packages/frontend/src/types/app.ts
+++ packages/frontend/src/types/app.ts
@@ -1,12 +1,17 @@
-export type PaintColour = 'blue' | 'grey' | 'black' | 'white' | 'purple';
+export type PaintColour = 'blue' | 'gray' | 'black' | 'white' | 'purple';
 
 export type Paint = {
   name: PaintColour,
   stockStatus: 'available' | 'running-low' | 'out-of-stock'
 };
 
+export type Resources = 'paint-colours' | 'users';
+
+export type Actions = 'view' | 'update';
+
 export type User = {
   username: string,
   name: string,
   role: 'painter' | 'inventory-manager' | 'system-admin',
+  permissions: Record<Resources, Actions[]>
 };
