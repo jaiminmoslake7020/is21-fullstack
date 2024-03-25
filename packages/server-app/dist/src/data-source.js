8f9260d6a9922e9e61e15a98c452e47303b9f737
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-03-25 05:00:41 -0700

    server app

--- packages/server-app/dist/src/data-source.js
+++ packages/server-app/dist/src/data-source.js
@@ -3,15 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
-const Product_1 = require("./models/Product");
-const Developer_1 = require("./models/Developer");
-const ProductDeveloper_1 = require("./models/ProductDeveloper");
+const PaintColour_1 = require("./models/PaintColour");
+const User_1 = require("./models/User");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "sqlite",
     database: "./server.sqlite",
     synchronize: true,
     logging: false,
-    entities: [Product_1.Product, Developer_1.Developer, ProductDeveloper_1.ProductDeveloper],
+    entities: [PaintColour_1.PaintColour, User_1.User],
     migrations: [],
     subscribers: [],
 });
--- packages/server-app/dist/src/index.js
+++ packages/server-app/dist/src/index.js
@@ -4,10 +4,9 @@ const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
 const body_parser_1 = tslib_1.__importDefault(require("body-parser"));
 const data_source_1 = require("./data-source");
-const productRoutes_1 = tslib_1.__importDefault(require("./routes/productRoutes"));
+const paintColoursRoutes_1 = tslib_1.__importDefault(require("./routes/paintColoursRoutes"));
 const swagger_1 = tslib_1.__importDefault(require("./utils/swagger"));
-const healthRoute_1 = tslib_1.__importDefault(require("./routes/healthRoute"));
-const developerRoutes_1 = tslib_1.__importDefault(require("./routes/developerRoutes"));
+const userRoutes_1 = tslib_1.__importDefault(require("./routes/userRoutes"));
 const cors_1 = tslib_1.__importDefault(require("cors"));
 data_source_1.AppDataSource.initialize().then(() => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     // create express app
@@ -19,9 +18,8 @@ data_source_1.AppDataSource.initialize().then(() => tslib_1.__awaiter(void 0, vo
     const port = 3000;
     // start express server
     app.listen(port, () => {
-        (0, healthRoute_1.default)(app);
-        (0, productRoutes_1.default)(app);
-        (0, developerRoutes_1.default)(app);
+        (0, paintColoursRoutes_1.default)(app);
+        (0, userRoutes_1.default)(app);
         (0, swagger_1.default)(app, port);
     });
 })).catch(error => console.log(error));
